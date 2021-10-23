23019ec7ed7a4a3e6b61e81d2424c0c15b49a906
guadalupe <guadalupe.rhinojosa@alumnos.udg.mx>
2021-10-23 17:31:04 -0500

    update files pacjage-lock.json, package.json, conecction.ts

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+require('dotenv').config();
+
 //conectando a base de datos
 export const usersConnection = mongoose.createConnection(
     process.env.DB_URL_USER ||'mongodb://localhost/db_users');
